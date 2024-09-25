d7bef85d000a34353d64ee37d1b277775894bb1a
Sabridh <sabri.dh@icloud.com>
2024-09-25 10:24:19 +0200

    Update m2.mjs

--- m2.mjs
+++ m2.mjs
@@ -1,4 +1,3 @@
-let verschil;
 import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
@@ -6,11 +5,11 @@ const userInput = readline.createInterface({input, output});
 let geboortejaar = await userInput.question('in welke jaar ben je geboren? ');
 let toekomstjaar = await userInput.question('in welke jaar wil je het berekenen? ');
 
-
-
+let som;
+let verschil;
 
 verschil = toekomstjaar - geboortejaar;
+som = verschil + 1
 
 
-
-console.log('in '+ toekomstjaar +', zal ik '+ verschil +' of '+ verschil +' jaar oud zijn');
+console.log('in '+ toekomstjaar +', zal ik '+ verschil +' of '+ som +' jaar oud zijn');
